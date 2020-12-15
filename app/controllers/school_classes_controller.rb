e419a1410e5135b14890d8f76ef810696caf0d03
Zoe Kirsh <zoekirsh@gmail.com>
2020-12-14 20:54:17 -0800

    minor cleaning

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -19,7 +19,7 @@ class SchoolClassesController < ApplicationController
   def update
     @school_class = SchoolClass.find(params[:id])
     @school_class.update(school_class_params)
-    redirect_to school_class_path(@school_class)
+    redirect_to @school_class
   end
 
   private
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -19,7 +19,7 @@ class StudentsController < ApplicationController
   def update
     @student = Student.find(params[:id])
     @student.update(student_params)
-    redirect_to student_path(@student)
+    redirect_to @student
   end
 
   private
